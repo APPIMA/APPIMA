a105b327e34dc4a7174ad7766594665c6a19f70c
Alejandro Ivan Lizarraga Rojas <alejandroivanliza@gmail.com>
2023-07-24 20:19:46 -0700

    Añadir mas colores

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
-import React from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
+import React from "react";
 import Colors from "./src/Colors";
 import BarTitle from "./src/Components/BarTitle/BarTitle";
 import Devices from "./src/Components/Devices/Devices";
@@ -8,18 +8,17 @@ import GlobalProvider from "./src/Components/Context/GlobalProvider";
 
 export default function App() {
   return (
-    <View  style={styles.container}>
+    <View style={styles.container}>
       <GlobalProvider>
         <StatusBar
           hidden={false}
           barStyle="default"
           animated={false}
           translucent={false}
-          backgroundColor={Colors.white}
+          backgroundColor={Colors.darkBlue}
         />
         <BarTitle name="Menu" />
         <Devices />
-        <Text>APP LEVANTADA</Text>
       </GlobalProvider>
     </View>
   );
@@ -27,7 +26,7 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: Colors.white,
+    backgroundColor: Colors.background,
     alignItems: "flex-start",
     justifyContent: "center",
   },
--- src/Colors/index.js
+++ src/Colors/index.js
@@ -1,6 +1,15 @@
 const Colors = {
   white: '#fff',
-  gray: '#808080'
+  gray: '#808080',
+  green: '#007f5f',
+  orange: '#f77f00',
+  red: '#ef233c',
+  darkBlue: '#0077b6',
+  lightGray: "#dee2e6",
+  background: '#e9ecef',
+  lighterBlue: '#014f86',
+  text: '#e7ecef',
+  darkText: '#212529'
 };
 
 export default Colors;
