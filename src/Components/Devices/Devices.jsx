a37e2634f36594b875a8cf782ee59bf79ad3f04e
Alejandro Ivan Lizarraga Rojas <alejandroivanliza@gmail.com>
2023-07-26 21:23:43 -0700

    Codigo para actualizar lecturas

--- src/Components/Devices/Devices.jsx
+++ src/Components/Devices/Devices.jsx
@@ -1,21 +1,58 @@
-import React, { useContext } from "react";
+import React, { useCallback, useContext, useEffect } from "react";
 import { ScrollView, StyleSheet } from "react-native";
-import GlobalContext from "../Context/GlobalContext";
+import GlobalContext from "../../Context/GlobalContext";
 import Device from "./Device";
 
 export default function Devices() {
-  const { devices } = useContext(GlobalContext);
+  const { devices, setDevices } = useContext(GlobalContext);
 
-  return <ScrollView style={styles.devicesContainer}>
-    {devices.map((device) => (
-      <Device key={device.id} {...device} />
-    ))}
-  </ScrollView>
+  const updateDevices = useCallback(() => {
+    const updatedDevices = devices.map((device) => {
+      const updatedDevice = { ...device };
+
+      const client = new WebSocket(`ws://${device.host}:${device.port}`);
+
+      client.onmessage = (e) => {
+        try {
+          const jsonData = JSON.parse(e.data);
+          updatedDevice.lectura1 = jsonData.sensor1;
+          updatedDevice.lectura2 = jsonData.sensor2;
+          updatedDevice.lectura3 = jsonData.sensor3;
+          updatedDevice.lectura4 = jsonData.sensor4;
+          updatedDevice.lastUpdate = Date.now();
+        } catch (error) {
+          console.log("Texto no es un JSON");
+        }
+      };
+
+      client.close();
+
+      return updatedDevice;
+    });
+
+    setDevices(updatedDevices);
+  }, [devices, setDevices]);
+
+  useEffect(() => {
+    const intervalo = setInterval(updateDevices, 1000);
+
+    return () => {
+      clearInterval(intervalo);
+    };
+  });
+
+  return (
+    <ScrollView style={styles.devicesContainer}>
+      {devices.map((device) => (
+        <Device key={device.id} {...device} />
+      ))}
+    </ScrollView>
+  );
 }
 
 const styles = StyleSheet.create({
   devicesContainer: {
     marginBottom: 48,
-    paddingTop: 16
+    paddingTop: 16,
   },
 });
