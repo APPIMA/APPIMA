501427c9eeb0f004276293805bcd810af2f3cb39
Alejandro Ivan Lizarraga Rojas <alejandroivanliza@gmail.com>
2023-07-31 13:03:24 -0700

    Mover añadir dispositivo fuera de la barra de titulo

---
+++ src/Components/BarTitle/AddDevice.jsx
@@ -0,0 +1,128 @@
+import {
+  View,
+  TouchableOpacity,
+  Modal,
+  StyleSheet,
+  Dimensions,
+  TextInput,
+} from "react-native";
+import React, { useContext, useState } from "react";
+import { Ionicons } from "@expo/vector-icons";
+
+import useForm from "../../hooks/useForm";
+
+import DevicesContext from "../../Context/DevicesContext";
+import Colors from "../../Colors";
+import CustomText from "../ui/CustomText";
+
+export default function AddDevice() {
+  const { addDevice } = useContext(DevicesContext);
+
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const { deviceName, host, port, onInputChange, onResetForm } = useForm({
+    deviceName: "",
+    host: "",
+    port: "",
+  });
+
+  const toggleModal = () => {
+    setIsModalVisible((prev) => !prev);
+  };
+
+  const handleSave = () => {
+    toggleModal();
+    addDevice({ deviceName, port, host });
+    onResetForm();
+  };
+
+  return (
+    <View>
+      <TouchableOpacity onPress={toggleModal} style={styles.button}>
+        <Ionicons name="add" size={28} color={Colors.text} />
+      </TouchableOpacity>
+
+      <Modal visible={isModalVisible} transparent animationType="fade">
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <TextInput
+              style={styles.input}
+              onChangeText={(value) =>
+                onInputChange({ value, name: "deviceName" })
+              }
+              placeholder="Escriba el nombre"
+            />
+            <TextInput
+              style={styles.input}
+              onChangeText={(value) => onInputChange({ value, name: "host" })}
+              placeholder="Escriba la dirección IP"
+            />
+            <TextInput
+              style={styles.input}
+              onChangeText={(value) => onInputChange({ value, name: "port" })}
+              placeholder="Escriba el puerto"
+            />
+            <TouchableOpacity onPress={handleSave} style={styles.saveButton}>
+              <CustomText style={styles.saveButtonText}>Guardar</CustomText>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={toggleModal} style={styles.cancelButton}>
+              <CustomText style={styles.cancelButtonText}>Cancelar</CustomText>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    borderRadius: 100,
+    borderColor: Colors.text,
+    borderWidth: 1,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    width: 42,
+    height: 42,
+  },
+
+  // Modal styles
+  modalContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: Colors.background,
+  },
+  modalContent: {
+    backgroundColor: Colors.white,
+    padding: 16,
+    borderRadius: 8,
+    width: Dimensions.get("window").width - 32,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: Colors.lightGray,
+    borderRadius: 4,
+    padding: 8,
+    marginBottom: 12,
+  },
+  saveButton: {
+    backgroundColor: Colors.darkBlue,
+    paddingVertical: 12,
+    alignItems: "center",
+    borderRadius: 4,
+  },
+  saveButtonText: {
+    color: Colors.white,
+    fontSize: 16,
+  },
+  cancelButton: {
+    marginTop: 8,
+    paddingVertical: 12,
+    alignItems: "center",
+  },
+  cancelButtonText: {
+    color: Colors.darkText,
+    fontSize: 16,
+  },
+});
--- src/Components/BarTitle/BarTitle.jsx
+++ src/Components/BarTitle/BarTitle.jsx
@@ -1,71 +1,14 @@
-import {
-  View,
-  StyleSheet,
-  TouchableOpacity,
-  Modal,
-  TextInput,
-  Dimensions,
-} from "react-native";
-import React, { useContext, useState } from "react";
-import { Ionicons } from "@expo/vector-icons";
+import { View, StyleSheet } from "react-native";
+import React from "react";
 import Colors from "../../Colors";
-import DevicesContext from "../../Context/DevicesContext";
 import CustomText from "../ui/CustomText";
+import AddDevice from "./AddDevice";
 
 export default function BarTitle({ barTitle }) {
-  const { addDevice } = useContext(DevicesContext);
-
-  const [isModalVisible, setIsModalVisible] = useState(false);
-  const [deviceName, setDeviceName] = useState("");
-  const [host, setHost] = useState("");
-  const [port, setPort] = useState("");
-
-  const toggleModal = () => {
-    setIsModalVisible((prev) => !prev);
-  };
-
-  const handleSave = () => {
-    toggleModal();
-    addDevice({ deviceName, port, host });
-    setDeviceName("");
-    setHost("");
-    setPort("");
-  };
-
   return (
     <View style={styles.bar}>
       <CustomText style={styles.title}>{barTitle}</CustomText>
-      <TouchableOpacity onPress={toggleModal} style={styles.button}>
-        <Ionicons name="add" size={28} color={Colors.text} />
-      </TouchableOpacity>
-
-      <Modal visible={isModalVisible} transparent animationType="fade">
-        <View style={styles.modalContainer}>
-          <View style={styles.modalContent}>
-            <TextInput
-              style={styles.input}
-              onChangeText={setDeviceName}
-              placeholder="Escriba el nombre"
-            />
-            <TextInput
-              style={styles.input}
-              onChangeText={setHost}
-              placeholder="Escriba la dirección IP"
-            />
-            <TextInput
-              style={styles.input}
-              onChangeText={setPort}
-              placeholder="Escriba el puerto"
-            />
-            <TouchableOpacity onPress={handleSave} style={styles.saveButton}>
-              <CustomText style={styles.saveButtonText}>Guardar</CustomText>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={toggleModal} style={styles.cancelButton}>
-              <CustomText style={styles.cancelButtonText}>Cancelar</CustomText>
-            </TouchableOpacity>
-          </View>
-        </View>
-      </Modal>
+      <AddDevice />
     </View>
   );
 }
@@ -82,62 +25,12 @@ const styles = StyleSheet.create({
     paddingHorizontal: 10,
     backgroundColor: Colors.darkBlue,
     elevation: 10,
-    height: 56
+    height: 56,
   },
   title: {
     fontSize: 32,
     textAlign: "center",
     fontWeight: "600",
     color: Colors.text,
-  },
-  button: {
-    borderRadius: 100,
-    borderColor: Colors.text,
-    borderWidth: 1,
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    width: 42,
-    height: 42,
-  },
-
-  // Modal styles
-  modalContainer: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: Colors.background,
-  },
-  modalContent: {
-    backgroundColor: Colors.white,
-    padding: 16,
-    borderRadius: 8,
-    width: Dimensions.get("window").width - 32,
-  },
-  input: {
-    borderWidth: 1,
-    borderColor: Colors.lightGray,
-    borderRadius: 4,
-    padding: 8,
-    marginBottom: 12,
-  },
-  saveButton: {
-    backgroundColor: Colors.darkBlue,
-    paddingVertical: 12,
-    alignItems: "center",
-    borderRadius: 4,
-  },
-  saveButtonText: {
-    color: Colors.white,
-    fontSize: 16,
-  },
-  cancelButton: {
-    marginTop: 8,
-    paddingVertical: 12,
-    alignItems: "center",
-  },
-  cancelButtonText: {
-    color: Colors.darkText,
-    fontSize: 16,
   },
 });
